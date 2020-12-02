f58f7f20fc785f8ae9e7936bfdd9b28e057e3a6f
Felipe Glaría Grego <fglaria@sicom.cl>
2020-12-02 12:33:33 -0300

    FIFO library added, with uint8_t buffer support

---
+++ fifo.c
@@ -0,0 +1,95 @@
+/*
+ * utils.c
+ *
+ *  Created on: 01-12-2020
+ *      Author: felipe
+ */
+
+#include <Manager/fifo.h>
+
+
+void fifo8_init(volatile fifo8_t *fifo, volatile uint8_t * const buffer, const uint8_t size)
+{
+    fifo->buffer = buffer;
+    fifo->first = buffer;
+    fifo->last = buffer;
+
+    fifo->queued = 0;
+    fifo->max_size = size;
+
+    return;
+}
+
+bool fifo8_get_byte(volatile fifo8_t * const fifo, uint8_t * const byte)
+{
+    // Check if input FIFO has bytes to check
+    if (0 < fifo->queued)
+    {
+        // Pull first byte of FIFO
+        *byte = *fifo->first;
+        --fifo->queued;
+
+        // Move first pointer to next byte to check, checking end of buffer
+        if (&fifo->buffer[fifo->max_size] == fifo->first)
+        {
+            fifo->first = fifo->buffer;
+        }
+        else
+        {
+            ++fifo->first;
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+
+void fifo8_push(volatile fifo8_t *fifo, const uint8_t byte)
+{
+    // Check if input FIFO is full
+    if (fifo->max_size > fifo->queued)
+    {
+        // Not full, push byte
+        *fifo->last = byte;
+        ++fifo->queued;
+
+        // Point last to next available address, checking end of buffer
+        if (&fifo->buffer[fifo->max_size] == fifo->last)
+        {
+            fifo->last = fifo->buffer;
+        }
+        else
+        {
+            ++fifo->last;
+        }
+    }
+    else
+    {
+        // FIFO is full, oldest byte (first) has to be overwritten
+        // First and last byte pointing to same location
+        // Move first byte to next one, checking end of buffer
+        if (&fifo->buffer[fifo->max_size] == fifo->first)
+        {
+            fifo->first = fifo->buffer;
+        }
+        else
+        {
+            ++fifo->first;
+        }
+
+        // Push byte
+        *fifo->last = byte;
+
+        // Move last byte to same address of first byte
+        // Should be processed before next new byte is pushed
+        fifo->last = fifo->first;
+    }
+
+    return;
+}
+
+
+/*** end of file ***/
+
---
+++ fifo.h
@@ -0,0 +1,73 @@
+/** \file utils.h
+ *  \brief FIFO library header file
+ *  This library enables use of buffers as circular FIFOs
+ *
+ *  \date 01/12/2020
+ *  \author Felipe Glaria
+ */
+
+#ifndef _FIFO_H_
+#define _FIFO_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+//*****************************************************************************
+// defines
+//*****************************************************************************
+
+
+//*****************************************************************************
+// typedefs
+//*****************************************************************************
+
+/** \struct fifo8_t
+ *  Structure to use uint8_t buffer as FIFO
+ */
+typedef struct {
+    volatile uint8_t *buffer;
+    volatile uint8_t *first;
+    volatile uint8_t *last;
+    volatile uint8_t queued;
+    volatile uint8_t max_size;
+} fifo8_t;
+
+
+//*****************************************************************************
+// globals
+//*****************************************************************************
+
+
+//*****************************************************************************
+// the function prototypes
+//*****************************************************************************
+
+/** \brief Function to initialize fifo8_t to use uint8_t buffer as FIFO
+ *
+ * \param fifo      Pointer to fifo8_t structure
+ * \param buffer    Buffer to uint8_t buffer to use as FIFO
+ * \param max_size  Size of buffer
+ */
+void fifo8_init(volatile fifo8_t * const fifo, volatile uint8_t * const buffer, const uint8_t size);
+
+/** \brief Function to get byte on FIFO
+ *
+ * \param fifo      Pointer to fifo8_t structure
+ * \param byte      Pointer to byte to write output
+ *
+ * \return  true if byte was pulled from FIFO; false if no byte on FIFO
+ */
+bool fifo8_get_byte(volatile fifo8_t * const fifo, uint8_t * const byte);
+
+/** \brief Function to push byte on FIFO
+ *
+ * \param fifo      Pointer to fifo8_t structure
+ * \param byte      Byte to push on FIFO
+ */
+void fifo8_push(volatile fifo8_t * const fifo, const uint8_t byte);
+
+
+#endif /* _FIFO_H_ */
+
+/*** end of file ***/
+
